d619d4590fcada9278bad9b9358bc58343e22665
Imantium <Ryan.Perrigo@gmail.com>
2020-04-22 15:54:47 -0400

    added order summary button and functionality on remove buttons

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,18 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        canBuy: false
+        }
+
+        updatePurchaseState (ingredients) {
+    
+            const sum = Object.keys(ingredients).map(igKey => {
+                return ingredients[igKey]
+            }).reduce((sum, el) => {
+                return sum + el
+            },0);
+            this.setState({canBuy: sum > 0});
         }
     
     addIngredientHandler = (type) => {
@@ -37,20 +48,41 @@ class BurgerBuilder extends Component {
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
     
     };
     removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) { return};
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = { ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
+    }
 
 
-    };
-    
     render () {
+    const disabledInfo ={
+                ...this.state.ingredients
+            };
+            for (let key in disabledInfo) {
+                disabledInfo[key] = disabledInfo[key] <=0
+            }
         return(
             <Auxil>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                ingredientAdded={this.addIngredientHandler}/>
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}
+                canBuy={this.state.canBuy}
+                price={this.state.totalPrice}/>
 
             </Auxil>
         );
